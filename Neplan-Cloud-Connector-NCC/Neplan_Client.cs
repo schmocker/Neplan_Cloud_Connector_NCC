08e9f22557d8006500f85e71f36320a66b589333
schmocker <33063045+schmocker@users.noreply.github.com>
2018-01-14 20:02:08 +0100

    make a release in vs

--- Neplan-Cloud-Connector-NCC/Neplan_Client.cs
+++ Neplan-Cloud-Connector-NCC/Neplan_Client.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+//////////using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
