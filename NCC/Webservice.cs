051fe41d625aec5dbe6111513543a5c8f81cd1eb
agittarius <33063045+schmocker@users.noreply.github.com>
2018-04-23 10:27:51 +0200

    restructure the whole solution
    instead of connecting over mqtt, now theres a dll as NCC. There is no more console application. One can load the NCC.dll in your project and use it there. It's much easier and faster this way.

---
+++ NCC/Webservice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Text;
+
+namespace NCC
+{
+    public class Webservice : NeplanServiceClient
+    {
+
+        public NeplanServiceClient nepService;
+
+        public Webservice(String ws_adress, String username, String password)
+            : base(GetBinding(), new EndpointAddress(ws_adress))
+        {
+            this.ClientCredentials.UserName.UserName = username;
+            this.ClientCredentials.UserName.Password = getMd5Hash(password);
+        }
+
+        private static WSHttpBinding GetBinding()
+        {
+            WSHttpBinding binding = new WSHttpBinding();
+            binding.Name = "WSHttpBinding_NeplanService";
+            binding.Security.Mode = SecurityMode.TransportWithMessageCredential;
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.None;
+            binding.Security.Message.ClientCredentialType = MessageCredentialType.UserName;
+            return binding;
+        }
+
+        private static string getMd5Hash(string input)
+        {
+            #region Not important for the training
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5 md5Hasher = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+            #endregion
+        }
+        public Dictionary<string, Object> GetMethodSpecifications(string MethodName)
+        {
+            MethodInfo method = this.GetType().GetMethod(MethodName);
+            ParameterInfo[] parameters = method.GetParameters();
+
+            Dictionary<string, Object> mehodSpecification = new Dictionary<string, Object>();
+
+            Dictionary<string, Object>[] Inputs = new Dictionary<String, Object>[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Inputs[i] = new Dictionary<string, Object>();
+                Inputs[i].Add("Name", parameters[i].Name);
+                Inputs[i].Add("Type", parameters[i].ParameterType.ToString());
+            }
+            mehodSpecification.Add("Inputs", Inputs);
+            mehodSpecification.Add("Output", method.ReturnType.ToString());
+            return mehodSpecification;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Neplan-Cloud-Connector-NCC
-This is a program to connect to the Neplan Cloud
+This is a dll to connect to the Neplan Cloud
 All About the Neplan Cloud: http://www.neplan.ch/neplanproduct/de-neplan-360-cloud
 
-The program allows any other program to send neplan commands and inputs via mqtt and to get the results also via mqtt. Like this, every program in any programming language can communicate to the Neplan Cloud.
-
-So far I made a client class for Matlab: https://github.com/schmocker/NCC-Matlab-Client
-Similar to the Matlab class one could write a class for Python, Java or any other programming language.
+The dll allows any other program to send neplan commands and inputs to Neplan Cloud and to get the results. In most programming languages dll's can be imported and used without much code.
 
 ## Requirements
-* Mqtt-Broker
 * Neplan Cloud Login
+* Neplan Cloud Webservices module has to be activated by Neplan
 
-There are many free and ready to use Mqtt-Brokers available. For example: iot.eclipse.org with port 1883
+## Howto
+```matlab
+NCC_dll = NET.addAssembly('path/NCC.dll');
 
+url =		"https://demo.neplan.ch/NEPLAN360_Demo/Services/External/NeplanService.svc";
+username =	"username";
+pasword =	"password";
+project_name =	"Neplan_Project_X";
 
-
-## Required NuGet packages:
-* M2MqttDotnetCore by Hamidreza Mohaqeq (>1.0.7)
-* Newtonsoft.Json by James Newton-King (>10.0.3)
+ws = NCC.Webservice(url,username,password);
+ws.Open();
+project = ws.GetProject(project_name, "", "", "");
+```
